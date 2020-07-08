194b974e348c8452d1b7d10677d6f27660f90d81
Mateo Periago Serrano <mateo@meligrana.com>
2020-07-09 01:57:30 +0200

    Added a TON of commands

--- boxes.py
+++ boxes.py
@@ -3,13 +3,20 @@ class Object():
         self.name = name
         self.desc = desc
         self.tags = tags
-        self.guid = 0
+        self.id = 0
 
     def __str__(self):
-        return '[O] ' + self.name
+        return '[O_' + str(self.id) + '] ' + self.name
+
+    def details(self):
+        return self.name + ' [Box]\nDescription: ' + self.desc + \
+    '\nTags: ' + ', '.join(self.tags)
 
     def as_dict(self, recursive=False):
         return {'name': self.name, 'desc': self.desc, 'tags': self.tags}
+
+    def delete(self):
+        del self
 
 class Box(Object):
     def __init__(self, name, desc='', tags=[]):
@@ -18,9 +25,14 @@ class Box(Object):
 
     def add_things(self, t):
         self.things.append(t)
+        print('Added', t, 'to', self)
 
     def __str__(self):
-        return '[B] ' + self.name
+        return '[B_' + str(self.id) + '] ' + self.name
+
+    def details(self):
+        return '\n' + self.name + ' [Box]\nDescription: ' + self.desc + \
+    '\nTags: ' + ', '.join(self.tags) + '\n'
 
     def as_dict(self, recursive=False):
         d = super().as_dict()
@@ -31,24 +43,49 @@ class Box(Object):
         d['things'] = ts
         return d
 
-    def get_all_children(self, mx_lvl=0, level=0):
+    def get_all_children(self, max_lvl=0, level=0):
         res = []
         for t in self.things:
             res.append(t)
-            if type(t) is Box and (mx_lvl > level or mx_lvl==-1):
+            if type(t) is Box and (max_lvl > level or max_lvl==-1):
                 res += t.get_all_children(level + 1)
         return res
 
-
+    def delete_child(self, thing):
+        if thing in self.things:
+            self.things.remove(thing)
 
 class BoxManager():
     def __init__(self, db):
-        self.root = None
+        self.set_root()
         self.db = db
+        self.last_id = 0
 
-    def from_guid(self, guid):
-        return self.db.search(Query().guid == guid)
+    def set_root(self, r=None):
+        self.root = r
+        self.reload()
 
+    def reload(self):
+        if self.root:
+            count = 0
+            for o in self.get_all():
+                o.id = count
+                count += 1
+
+    def get_all(self):
+        return [self.root] + self.root.get_all_children(-1)
+            
+    def from_id(self, _id):
+        l = [o for o in self.get_all() if o.id == int(_id)]
+        return l[0] if l else None
+
+    def delete(self, _id):
+        o = self.from_id(_id)
+        for i in self.get_all():
+            if type(i) is Box:
+                i.delete_child(o)
+            
+    
     def traverse_all(self):
         self.traverse_box(self.root)
 
@@ -63,13 +100,14 @@ class BoxManager():
     def search(self, name='', desc='', tags=[]):
         results = [self.root] + self.root.get_all_children(-1)
         # Name
-        f_name = [x for x in results if name.lower() == x.name.lower()] if name != '' else results
+        f_name = [x for x in results if name.lower() in x.name.lower()] if name != '' else results
         # Desc
         f_desc = [x for x in results if desc.lower() in x.desc.lower()] if desc != '' else results
         # Tags
-        f_tag = [x for x in results if any([y for y in tags if y in x.tags])] if len(tags) > 0 else results
+        f_tag = [x for x in results if all([y in x.tags for y in tags])] if len(tags) > 0 else results
+
         results = [item for item in results
-                   if item in f_tag and
-                   item in f_name and
+                   if item in f_name and
+                   item in f_desc and
                    item in f_tag]
         return results
--- commands.py
+++ commands.py
@@ -1,8 +1,8 @@
-import re
+import re, warnings, copy
 import boxes
 from boxes import Object, Box, BoxManager
-import warnings
 from tinydb import TinyDB, Query
+
 
 class Command():
     def __init__(self, bm):
@@ -16,6 +16,9 @@ class Command():
     
     def run(self, x):
         miss = [r for r in self.req if r not in x.keys()]
+        if 'h' in x.keys():
+            self.help() # ?
+            return True
         if len(miss) > 0:
             warnings.warn('[ERROR] Missing required argument(s): ' + ','.join(miss))
             return False
@@ -24,12 +27,10 @@ class Command():
 class ls(Command):
     def run(self, x):
         if super().run(x):
-            print('ls', x)
             if len(x) == 0:
                 self.bm.traverse_all()
             else:
-                b = self.bm.search(x['d'])
-                self.bm.traverse_box(x)
+                self.bm.traverse_box(self.bm.from_id(x['i']))
         
 class mkbox(Command):
     def __init__(self, bm):
@@ -45,29 +46,164 @@ class mkbox(Command):
             else: d = ''
             ret = Box(x['n'], desc=d)
             if 'i' in x.keys():
-                dest = bm.from_guid(x['i'])
+                dest = bm.from_id(x['i'])
+                if type(dest) is not Box:
+                    warnings.warn('[ERROR] The given ID corresponds to an Object, not a Box.')
+                    dest = self.bm.root
             else:
                 dest = self.bm.root
-            dest.add_things(dest)
-            print('Added', ret, 'to', dest)
+            dest.add_things(ret)
+            bm.reload()
 
-class tag(Command):
+class mkobj(Command):
+    def __init__(self, bm):
+        super().__init__(bm)
+        self.req = ['n']
+        self.help = '''Create a new object.
+--n: Name of the object.
+--i: parent box of the object. Default is root.'''
+    
     def run(self, x):
         if super().run(x):
-            pass
+            if 'd' in x.keys(): d = x['d']
+            else: d = ''
+            ret = Object(x['n'], desc=d)
+            if 'i' in x.keys():
+                dest = bm.from_id(x['i'])
+                if type(dest) is not Box:
+                    warnings.warn('[ERROR] The given ID corresponds to an Object, not a Box.')
+                    dest = self.bm.root
+            else:
+                dest = self.bm.root
+            dest.add_things(ret)
+            bm.reload()
 
-class edit(Command):
-    def __init__(self):
+class tag(Command):
+    def __init__(self, bm):
+        super().__init__(bm)
         self.req = ['i']
     
     def run(self, x):
         if super().run(x):
+            b = bm.from_id(x['i'])
+            if 't' in x.keys():
+                b.tags += parse_list(x['t'])
+
+class edit(Command):
+    def __init__(self, bm):
+        super().__init__(bm)
+        self.req = ['i']
+    
+    def run(self, x):
+        if super().run(x):
+            b = bm.from_id(x['i'])
+            if 'n' in x.keys():
+                b.name = x['n']
+            if 'd' in x.keys():
+                b.desc = x['d']
+            if 't' in x.keys():
+                b.tags = parse_list(x['t'])
+
+class rm(Command):
+    def __init__(self, bm):
+        super().__init__(bm)
+        self.req = ['i']
+        
+    def run(self, x):
+        if super().run(x):
+            bm.delete(x['i'])
+            bm.reload()
+            
+class cp(Command):
+    def __init__(self, bm):
+        super().__init__(bm)
+        self.req = ['i1']
+        
+    def run(self, x):
+        if super().run(x):
+            b1 = bm.from_id(x['i1'])
+            if 'i2' in x.keys():
+                dest = bm.from_id(x['i2'])
+                if type(dest) is not Box:
+                    warnings.warn('[ERROR] The given ID corresponds to an Object, not a Box.')
+                    dest = self.bm.root
+            else:
+                dest = self.bm.root
+
+            
+            if type(b1) is Box:
+                b2 = Box(b1.name, b1.desc, b1.tags)
+                things = b1.things.copy()
+            else:
+                b2 = Object(b1.name, b1.desc, b1.tags)
+            
+            dest.add_things(b2)
+            bm.reload()
+            if type(b2) is Box:
+                print('adding', b1, 'to', dest)
+                for t in things:
+                    cp(bm).run({'i1': t.id, 'i2': b2.id})
+                
+class mv(Command):
+    def __init__(self, bm):
+        super().__init__(bm)
+        self.req = ['i1', 'i2']
+
+    def run(self, x):
+        if super().run(x):
+            cp(bm).run(x)
+            rm(bm).run({'i': x['i1']})
+
+class info(Command):
+    def __init__(self, bm):
+        super().__init__(bm)
+        self.req = ['i']
+    
+    def run(self, x):
+        if super().run(x):
+            print('\n')
+            if 'i' in x.keys():
+                print(bm.from_id(x['i']).details())
+            print(path(bm).run(x))
+            print('\n')
+
+class path(Command):
+    def __init__(self, bm):
+        super().__init__(bm)
+        self.req = ['i']
+    
+    def run(self, x):
+        if super().run(x):
+            if 'i' in x.keys():
+                print(bm.from_id(x['i']).details())
+
+class search(Command):
+    def __init__(self, bm):
+        super().__init__(bm)
+    
+    def run(self, x):
+        if super().run(x):
+            n = d = ''
+            ts = []
+            di = {}
             if 'n' in x.keys():
                 n = x['n']
-            if 'd' in x.keys(): d = x['d']
-            else: d = ''
+                di['name'] = n
+            if 'd' in x.keys():
+                d = x['d']
+                di['description'] = d
+            if 't' in x.keys():
+                ts = parse_list(x['t'])
+                di['tags'] = ts
+            s = bm.search(name=n, desc=d, tags=ts)
+            
+            print('\nShowing results for ' + str(di) + ':')
+            print([str(x) for x in s])
 
-cmd_reg = '''(?P<CMD>^\S*)*(((?P<FLAGS>--\w{1}) (?P<VAL>\w+))*)'''
+cmd_reg = '''(?P<CMD>^\S*)*(((?P<FLAGS>--\w+) (?P<VAL>(\'(.+)\'|[^\s]+)))*)'''
+
+def parse_list(s):
+    return s.strip('][').split(', ')
 
 def parse_command(cmd):
     matches = re.finditer(cmd_reg, cmd, re.MULTILINE)
@@ -78,7 +214,7 @@ def parse_command(cmd):
         f = match.group('FLAGS')
         if f:
             f = f[2:] # remove dashes
-            flags[f] = match.group('VAL')
+            flags[f] = match.group('VAL').strip().strip("'")
     return (c, flags)
 
 def run_command(x):
@@ -87,27 +223,35 @@ def run_command(x):
 # TEST
 bm = BoxManager(TinyDB('boxes.json'))
 my_room = Box('My room')
-my_room.add_things(Object('Pencil'))
+my_room.add_things(Object('Red Pencil', desc='My writing pencil!', tags=['school']))
+my_room.add_things(Object('Blue Pencil', desc='My drawing pencil!', tags=['school']))
+my_room.add_things(Object('Pen', desc='For note taking!', tags=['school']))
 shelf = Box('Shelf', desc='On the left wall')
-shelf.add_things(Object('Book', desc='A nice book!'))
+shelf.add_things(Object('Book', desc='A nice book!', tags=['school', 'books']))
 my_room.add_things(shelf)
 
-bm.root = my_room
+bm.set_root(my_room)
 
 commands = {
     'ls': ls(bm),
-    'mkbox': mkbox(bm)
+    'mkbox': mkbox(bm),
+    'mkobj': mkobj(bm),
+    'tag': tag(bm),
+    'edit': edit(bm),
+    'info': info(bm),
+    'rm': rm(bm),
+    'mv': mv(bm),
+    'cp': cp(bm),
+    'search': search(bm),
+    'path': path(bm) # TODO
 }
 
-s = bm.search()
-print([str(x) for x in s])
-
 #bm.traverse_box(shelf)
+s = bm.search(name='pen', tags=['school'])
+print([str(x) for x in s])
 
 if __name__ == '__main__':
     while True:
-        break
-        workdir = '/'
         inp = input('Command: ')
         cmd = parse_command(inp)
         run_command(cmd)
