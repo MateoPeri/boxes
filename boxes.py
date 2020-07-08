12c3c56d89b7d8c6bf647b490e31a62d98b4b0b9
Mateo Periago Serrano <mateo@meligrana.com>
2020-07-08 17:28:17 +0200

    Commands, db base

--- boxes.py
+++ boxes.py
@@ -3,9 +3,13 @@ class Object():
         self.name = name
         self.desc = desc
         self.tags = tags
+        self.guid = 0
 
     def __str__(self):
         return '[O] ' + self.name
+
+    def as_dict(self, recursive=False):
+        return {'name': self.name, 'desc': self.desc, 'tags': self.tags}
 
 class Box(Object):
     def __init__(self, name, desc='', tags=[]):
@@ -18,37 +22,54 @@ class Box(Object):
     def __str__(self):
         return '[B] ' + self.name
 
+    def as_dict(self, recursive=False):
+        d = super().as_dict()
+        if recursive:
+            ts = [t.as_dict(recursive) for t in self.things]
+        else:
+            ts = [str(t) for t in self.things]
+        d['things'] = ts
+        return d
+
+    def get_all_children(self, mx_lvl=0, level=0):
+        res = []
+        for t in self.things:
+            res.append(t)
+            if type(t) is Box and (mx_lvl > level or mx_lvl==-1):
+                res += t.get_all_children(level + 1)
+        return res
+
 
 
 class BoxManager():
-    def __init__(self):
-        self.boxes = []
-        
-    def add_box(self, box):
-        exists = [box.name in b.name for b in self.boxes]
-        if not exists:
-            self.boxes.append(box)
+    def __init__(self, db):
+        self.root = None
+        self.db = db
+
+    def from_guid(self, guid):
+        return self.db.search(Query().guid == guid)
 
     def traverse_all(self):
-        for b in boxes:
-            traverse_box(b)
+        self.traverse_box(self.root)
 
     def traverse_box(self, lst, level=0, indent=4):
         print('    ' * (level - 1) + '+--- ' * (level > 0) + str(lst))
         for l in lst.things[:]:
             if type(l) is Box:
-                self.print_list(l, level + 1)
+                self.traverse_box(l, level + 1)
             else:
                 print(' ' * indent * level + '+--- ' + str(l))
 
-# TEST
-bm = BoxManager()
-my_room = Box('My room')
-my_room.add_things(Object('Pencil'))
-shelf = Box('Shelf', desc='On the left wall')
-shelf.add_things(Object('Book', desc='A nice book!'))
-my_room.add_things(shelf)
-
-bm.add_box(my_room)
-
-bm.traverse_box(shelf)
+    def search(self, name='', desc='', tags=[]):
+        results = [self.root] + self.root.get_all_children(-1)
+        # Name
+        f_name = [x for x in results if name.lower() == x.name.lower()] if name != '' else results
+        # Desc
+        f_desc = [x for x in results if desc.lower() in x.desc.lower()] if desc != '' else results
+        # Tags
+        f_tag = [x for x in results if any([y for y in tags if y in x.tags])] if len(tags) > 0 else results
+        results = [item for item in results
+                   if item in f_tag and
+                   item in f_name and
+                   item in f_tag]
+        return results
--- commands.py
+++ commands.py
@@ -1,30 +1,114 @@
 import re
+import boxes
+from boxes import Object, Box, BoxManager
+import warnings
+from tinydb import TinyDB, Query
 
-def ls(x):
-    print('ls')
-    return
-    if x == None:
-        bm.traverse_all()
-    else:
-        bm.traverse_box(x)
+class Command():
+    def __init__(self, bm):
+        self.req = []
+        self.help = ''
+        self.bm = bm
 
-commands = {
-    'ls [--d]': 'ls',
-    'mkbox --n [--d] [--]': 'mkbox'
-}
+    def help(self):
+        print(self.help)
+        print('Required Args: ' + ', '.join(self.req))
+    
+    def run(self, x):
+        miss = [r for r in self.req if r not in x.keys()]
+        if len(miss) > 0:
+            warnings.warn('[ERROR] Missing required argument(s): ' + ','.join(miss))
+            return False
+        return True
 
-cmd_reg = '''(?P<CMD>^\S*) (?P<FLAGS>--\w{1}) (?P<VAL>\w+)'''
+class ls(Command):
+    def run(self, x):
+        if super().run(x):
+            print('ls', x)
+            if len(x) == 0:
+                self.bm.traverse_all()
+            else:
+                b = self.bm.search(x['d'])
+                self.bm.traverse_box(x)
+        
+class mkbox(Command):
+    def __init__(self, bm):
+        super().__init__(bm)
+        self.req = ['n']
+        self.help = '''Create a new box.
+--n: Name of the box.
+--i: parent of the box. Default is root.'''
+    
+    def run(self, x):
+        if super().run(x):
+            if 'd' in x.keys(): d = x['d']
+            else: d = ''
+            ret = Box(x['n'], desc=d)
+            if 'i' in x.keys():
+                dest = bm.from_guid(x['i'])
+            else:
+                dest = self.bm.root
+            dest.add_things(dest)
+            print('Added', ret, 'to', dest)
+
+class tag(Command):
+    def run(self, x):
+        if super().run(x):
+            pass
+
+class edit(Command):
+    def __init__(self):
+        self.req = ['i']
+    
+    def run(self, x):
+        if super().run(x):
+            if 'n' in x.keys():
+                n = x['n']
+            if 'd' in x.keys(): d = x['d']
+            else: d = ''
+
+cmd_reg = '''(?P<CMD>^\S*)*(((?P<FLAGS>--\w{1}) (?P<VAL>\w+))*)'''
 
 def parse_command(cmd):
-    ms = re.findall(cmd_reg, cmd)
+    matches = re.finditer(cmd_reg, cmd, re.MULTILINE)
     flags = {}
-    for m in ms:
-        print(m)
-        if m.group() == 'FLAG':
-            flags.append(m)
+    for match in matches:
+        if match.group('CMD'):
+            c = match.group('CMD')
+        f = match.group('FLAGS')
+        if f:
+            f = f[2:] # remove dashes
+            flags[f] = match.group('VAL')
+    return (c, flags)
+
+def run_command(x):
+    commands[x[0]].run(x[1])
+
+# TEST
+bm = BoxManager(TinyDB('boxes.json'))
+my_room = Box('My room')
+my_room.add_things(Object('Pencil'))
+shelf = Box('Shelf', desc='On the left wall')
+shelf.add_things(Object('Book', desc='A nice book!'))
+my_room.add_things(shelf)
+
+bm.root = my_room
+
+commands = {
+    'ls': ls(bm),
+    'mkbox': mkbox(bm)
+}
+
+s = bm.search()
+print([str(x) for x in s])
+
+#bm.traverse_box(shelf)
 
 if __name__ == '__main__':
     while True:
+        break
         workdir = '/'
         inp = input('Command: ')
-        parse_command(inp)
+        cmd = parse_command(inp)
+        run_command(cmd)
+    
